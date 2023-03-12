9748f8bd11e61f0321525068c496e88920a3690e
yukazu1 <96405848+yukazu1@users.noreply.github.com>
2023-03-12 17:42:06 +0900

    Add files via upload

--- eitango/main.js
+++ eitango/main.js
@@ -7,13 +7,12 @@ var lang;//回答言語
 var past_num=[];//過去に出た問題を記録
 var false_num=[];//間違えた問題を記録
 var true_num=0;//正解数
-var r;
+var r=[];
 
 
 
 
 history.replaceState(null,null,"./")
-create();
 $('.modal_bg').click(function(){
   document.title='英語学習ならエイタンGO';
   $(this).fadeOut(200);
@@ -41,7 +40,7 @@ $('#modal').fadeIn(400);
 
 $('#next').click(function(){
 
-var tng_num = Number($('select option:selected').text());
+tng_num = Number($('select option:selected').text());
   if(tng_num==0){
     $(".error").css({"visibility":"visible"});
   }else{
@@ -77,7 +76,7 @@ $('.mod_3>div>button').click(function(){
     create();
 },800);
 });
-// create();
+
 function create(){
  $('.start_menu').html('<div class="loader"></div>');
 $('.modal_bg').click(); 
@@ -85,14 +84,20 @@ $('.modal_bg').click();
     .done(function (data) {
   json=data;
     random();
-    document.title='「'+json[r][1]+'」を英語で - エイタンGO';
+    document.title='「'+json[r[0]][1]+'」を英語で - エイタンGO';
     $('.start_menu').hide();
    $('#quiz').show();
-   $('#q').text('「'+json[r][1]+'」を英語で');
+   $('#q').text('「'+json[r[0]][1]+'」を英語で');
    var t=Math.floor(Math.random() * ((4 + 1) - 1)) + 1;
-   $('#b_'+t).text(json[r][0]);
-   console.log(r);
-past_num.push(r);
+   $('#b_'+t).text(json[r[0]][0]);
+   var a=1;
+   for(var i=1;i<5;i++){
+    if(i!==t){
+      $('#b_'+i).text(json[r[a]][0]);
+      a++
+    }
+   }
+past_num.push(r[0]);
   });
 }
 
@@ -113,6 +118,7 @@ $('#a>button').click(function(){
   }
   var url='?true='+true_num+'&false='+false_num.length;
   history.replaceState(null,null,url);
+
 });
 
 
@@ -121,10 +127,49 @@ $('.modal_quiz_bg').click(function(){
 $(this).hide();
 $('.modal_quiz>.false').hide();
 $('.modal_quiz>.true').hide();
+if(true_num+false_num.length==tng_num){
+  result();
+}else{
+  create();
+}
 
 })
 
 
 function random(){
- r = Math.floor(Math.random() * ((1000 + 1) - 1)) + 1;
+ var r1;
+ r=[];
+for(var i=0; i<1; i++){
+ r1=Math.floor(Math.random() * ((1000 + 1) - 1)) + 1;
+ if(past_num.indexOf(r1) !== -1){
+  i--;
+ }
+}
+r.push(r1);
+for(var i=0; i<3; i++){
+  rr=Math.floor(Math.random() * ((1000 + 1) - 1)) + 1;
+  if(past_num.indexOf(r1) === -1){
+    r.push(rr);
+}else{
+  i--;
+}
+}
+}
+
+
+function result(){
+  document.title='学習結果 - エイタンGO';
+  $('#quiz').hide();
+  $('.modal_quiz_bg').hide();
+  $('.start_menu').show();
+  $('#q_num').text(tng_num);
+  $('#t_num').text(true_num);
+  var per=Math.floor( true_num/tng_num * Math.pow( 10, 1 ) ) / Math.pow( 10, 1 ) 
+  $('#t_per').text(per);
+  $('#result>a').attr('href','https://twitter.com/intent/tweet?text=エイタンGOをやったら%0A正解率が'+per+'%25でした！(正解%3A'+true_num+'問、不正解%3A'+(tng_num-true_num+'問)%0Ayukazu211.github.io%2Feitango%2F%20%23エイタンGO'))
+  setTimeout(function(){
+    $('#result').show();
+    $('.start_menu').hide();
+    
+ },1000);
 }
